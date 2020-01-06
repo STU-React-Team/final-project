bb710b7feeb085344896db61c32d3f296941b887
pronamvip1998 <trannamhai.dn@gmail.com>
2020-01-06 14:13:49 +0700

    Test New Src lan 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import './App.css';
 const App: React.FC = () => (
 	<Provider store={store}>
 		<div className='App'>
-			<h1>React App Test</h1>
+			<h1>React App Test 2 fa-rotate-270</h1>
+			<h2>Hello</h2>
 		</div>
 	</Provider>
 );
